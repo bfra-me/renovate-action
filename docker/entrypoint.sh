eb520a7b38f1ab57089c1c1f1238add2dc89174d
Marcus R. Brown <contact@marcusrbrown.com>
2024-02-21 15:07:31 -0700

    feat: add Docker entrypoint script
    Install common tools that may be needed by `postUpgradeTasks` commands.

---
+++ docker/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# renovate: datasource=github-tags depName=mikefarah/yq
+export YQ_VERSION=v4
+
+apt update
+
+apt install -y curl jq
+
+curl -fsSL -o /usr/local/bin/yq https://github.com/mikefarah/yq/releases/download/${YQ_VERSION}/yq_linux_amd64
+chmod a+x /usr/local/bin/yq
+
+runuser -u ubuntu renovate
