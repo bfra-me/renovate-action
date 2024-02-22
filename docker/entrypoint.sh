b867712a069fdb3c501d99e14cb3b34f0060623e
Marcus R. Brown <contact@marcusrbrown.com>
2024-02-21 20:35:50 -0700

    fix: add fully-qualified path to the entrypoint script

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,10 +2,15 @@
 
 set -Eeuo pipefail
 
+# TODO: Remove debugging
+env | sort
+[ -d /tmp/renovate ] && ls -al /tmp/renovate
+
 # renovate: datasource=github-releases depName=mikefarah/yq
 export YQ_VERSION=v4.41.1
 
 curl -fsSL -o /usr/local/bin/yq https://github.com/mikefarah/yq/releases/download/${YQ_VERSION}/yq_linux_amd64
 chmod a+x /usr/local/bin/yq
+yq --version
 
 runuser -u ubuntu renovate
