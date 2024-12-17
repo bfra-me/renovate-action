31f81eb81c9615d4eaeb5c1b41783c8f9478311a
Marcus R. Brown <git@mrbro.dev>
2024-12-17 00:44:36 -0700

    chore: fix linter errors

--- src/wait.ts
+++ src/wait.ts
@@ -1,6 +1,6 @@
 export async function wait(milliseconds: number): Promise<string> {
   return new Promise(resolve => {
-    if (isNaN(milliseconds)) {
+    if (Number.isNaN(milliseconds)) {
       throw new TypeError('milliseconds not a number')
     }
 
