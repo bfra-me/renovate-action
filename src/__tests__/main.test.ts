d864f6dc77ea98af90b3f0c842ca3fb6cc77611e
Marcus R. Brown <git@mrbro.dev>
2025-02-18 22:59:45 -0700

    chore: move tests

--- src/__tests__/main.test.ts
+++ src/__tests__/main.test.ts
@@ -1,5 +1,5 @@
 import {expect, test} from 'vitest'
-import {wait} from '../src/wait'
+import {wait} from '../wait'
 import * as process from 'process'
 import * as cp from 'child_process'
 import * as path from 'path'
@@ -21,7 +21,7 @@ test('wait 500 ms', async () => {
 test('test runs', () => {
   process.env['INPUT_MILLISECONDS'] = '500'
   const np = process.execPath
-  const ip = path.join(__dirname, '..', 'dist', 'index.js')
+  const ip = path.join(__dirname, '../..', 'dist', 'index.js')
   const options: cp.ExecFileSyncOptions = {
     env: process.env,
   }
